50c7d054d74628bc776c917152e6b2774552e8f2
akimiyaersoz <kimyaersoz@icloud.com>
2025-02-28 04:59:43 +0000

    homework 11 new scripts

---
+++ Homework-11/input.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+read -p "enter your name: " name
+
+read -p  "enter your age: " age
+
+
+read -p "enter yout favorite language: " favorite language
+
+read -p "enter your hobby: " hobby
+
+read -p "enter your dream: " dream
+
+
+echo "Hi, my name is $name , and I'm $age  years old. Every morning, $name likes to spend time to $hobby .
+As a $age -year-old, I believe it's never too late to follow your dreams. My favorite programming language is $fav_language.
+I code in $fav_language almost every day!
+One day, I hope my love for $fav_language will help me achieve my dream of $dream ."
+
+echo  "When I'm not busy working toward $dream , you'll find me $hobby to relax."
+
+echo "That's a little about me, $name — a $age-year-old who loves $fav_language, enjoys $hobby , and dreams of $dream!"
---
+++ Homework-11/introduce.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+name=Aysel
+
+age=37
+
+fav_language=Persian
+
+hobby=music
+
+dream=travelling
+
+
+echo "Hi, my name is $name , and I'm $age  years old. Every morning, $name likes to spend time to $hobby .
+As a $age -year-old, I believe it's never too late to follow your dreams. My favorite programming language is $fav_language.
+I code in $fav_language almost every day!
+One day, I hope my love for $fav_language will help me achieve my dream of $dream ."
+
+echo  "When I'm not busy working toward $dream , you'll find me $hobby to relax."
+
+echo "That's a little about me, $name — a $age-year-old who loves $fav_language, enjoys $hobby , and dreams of $dream!"
+
+
+
+
